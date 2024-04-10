5258def8222e3698338168fbd276f31030b5edad
Mikuláš Kopecký <mikulas@ka.cz>
2024-04-10 17:23:29 +0200

    Endpoint creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { RecipesModule } from './recipes/recipes.module';
+import { RatingsModule } from './ratings/ratings.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, RecipesModule, RatingsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
