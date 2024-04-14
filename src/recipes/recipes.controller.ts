a0e420af375044b1bae67f3e698ed5e1cb481b7f
Mikuláš Kopecký <mikulas@ka.cz>
2024-04-15 00:53:53 +0200

    Recipes

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,20 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  Put,
+  UseGuards,
+  Req,
+  BadRequestException,
+} from '@nestjs/common';
 import { RecipesService } from './recipes.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('recipes')
 export class RecipesController {
   constructor(private readonly recipesService: RecipesService) {}
 
   @Post()
-  create(@Body() createRecipeDto: CreateRecipeDto) {
+  @UseGuards(AuthGuard)
+  create(@Req() request, @Body() createRecipeDto: CreateRecipeDto) {
+    createRecipeDto.ownerId = request.user.id;
     return this.recipesService.create(createRecipeDto);
   }
 
   @Get()
-  findAll() {
-    return this.recipesService.findAll();
+  findAll( @Query('take') take: number = 10, @Query('page') page: number = 1, @Query('userId') userId: number){
+    return this.recipesService.findAll(take, page, userId);
   }
 
   @Get(':id')
@@ -22,13 +38,17 @@ export class RecipesController {
     return this.recipesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
-    return this.recipesService.update(+id, updateRecipeDto);
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  update(@Req() req, @Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
+    return this.recipesService.update(+id, req.user.id, updateRecipeDto).then(
+      (result) => {return result;}, (error) => {throw error;}
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.recipesService.remove(+id);
+  @UseGuards(AuthGuard)
+  remove(@Param('id') id: string, @Req() req) {
+    return this.recipesService.remove(+id, req.user.id);
   }
 }
--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable, ParseIntPipe } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
+import { Recipe } from './entities/recipe.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RecipesService {
+  constructor(@InjectRepository(Recipe) private recipeRepository: Repository<Recipe>) {}
+
   create(createRecipeDto: CreateRecipeDto) {
-    return 'This action adds a new recipe';
+    return this.recipeRepository.save(createRecipeDto);
   }
 
-  findAll() {
-    return `This action returns all recipes`;
+  async findAll(takeRecipes: number, page: number, userId?: number) {
+    //https://stackoverflow.com/questions/53922503/how-to-implement-pagination-in-nestjs-with-typeorm#answer-67239246
+    const skip = (page - 1) * takeRecipes;
+    const [result, total] = await this.recipeRepository.findAndCount({
+      take: takeRecipes,
+      skip: skip,
+      select: ['id', 'title', 'ownerId'],
+      where: userId ? {ownerId: userId} : {}
+    });
+
+    return {
+      data: result,
+      count: total
+    }
+  }
+
+  //filters needs to have attributes that match the recipe entity, e.g. {title: 'recipe title'}
+  async filterRecipes(filters: any, takeRecipes: number, page: number) {
+    const skip = (page - 1) * takeRecipes;
+    const [result, total] = await this.recipeRepository.findAndCount({
+      take: takeRecipes,
+      skip: skip,
+      where: filters
+    });
+
+    return {
+      data: result,
+      count: total
+    }
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} recipe`;
+    return this.recipeRepository.findOne({where: {id: id}})
   }
 
-  update(id: number, updateRecipeDto: UpdateRecipeDto) {
-    return `This action updates a #${id} recipe`;
+  update(recipeId: number, userId: number, updateRecipeDto: UpdateRecipeDto) {
+    this.findOne(recipeId).then((recipe) => {
+      if(recipe.ownerId !== userId){
+        throw new BadRequestException('Not authorized to update this recipe');
+      }
+    });
+    return this.recipeRepository.update(recipeId, updateRecipeDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} recipe`;
+  async remove(recipeId: number, userId: number) {
+    await this.findOne(recipeId).then((recipe) => {
+      if(recipe.ownerId !== userId){
+        throw new HttpException('Not authorized to update this recipe', 403);
+      }
+    });
+    return this.recipeRepository.delete(recipeId);
   }
+
 }
