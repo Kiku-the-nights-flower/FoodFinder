0b0ccfc745a61697bc0781803bba1c8d8670858a
Mikuláš Kopecký <mikulas@ka.cz>
2024-04-15 00:53:43 +0200

    Rating

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,34 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { RatingsService } from './ratings.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('ratings')
+@UseGuards(AuthGuard)
 export class RatingsController {
   constructor(private readonly ratingsService: RatingsService) {}
 
   @Post()
-  create(@Body() createRatingDto: CreateRatingDto) {
-    return this.ratingsService.create(createRatingDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.ratingsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ratingsService.findOne(+id);
+  create(@Body() createRatingDto: CreateRatingDto, @Req() request) {
+    return this.ratingsService.create(createRatingDto, request.user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRatingDto: UpdateRatingDto) {
-    return this.ratingsService.update(+id, updateRatingDto);
+  update(@Param('id') id: string, @Body() updateRatingDto: UpdateRatingDto, @Req() request) {
+    return this.ratingsService.update(+id, updateRatingDto, request.user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ratingsService.remove(+id);
+  remove(@Param('id') id: string, @Req() request) {
+    return this.ratingsService.remove(+id, request.user);
   }
 }
--- src/ratings/ratings.service.ts
+++ src/ratings/ratings.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { Rating } from './entities/rating.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Recipe } from '../recipes/entities/recipe.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class RatingsService {
-  create(createRatingDto: CreateRatingDto) {
-    return 'This action adds a new rating';
+
+  constructor(
+    @InjectRepository(Rating) private ratingRep: Repository<Rating>,
+  ) {
   }
 
-  findAll() {
-    return `This action returns all ratings`;
+  create(createRatingDto: CreateRatingDto, user: User) {
+    this.ratingRep.findOne({where: {recipe: createRatingDto.recipeId, userId: user.id}}).then((rating) => {
+      if (rating) {
+        throw new BadRequestException('Rating already exists');
+      }
+    });
+
+    return this.ratingRep.insert(createRatingDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rating`;
+  update(id: number, updateRatingDto: UpdateRatingDto, userId: number) {
+    this.ratingRep.findOne({where: {id: id}}).then((rating) => {
+      if (!rating) {
+        throw new BadRequestException('Rating does not exist');
+      }
+      if (rating.userId !== userId) {
+        throw new BadRequestException('User is not the owner of the rating');
+      }
+    });
+    return this.ratingRep.update(id, updateRatingDto);
   }
 
-  update(id: number, updateRatingDto: UpdateRatingDto) {
-    return `This action updates a #${id} rating`;
-  }
+  remove(recipeId: number, userId: number) {
+    this.ratingRep.findOne({where: {id: recipeId}}).then((rating) => {
+      if (!rating) {
+        throw new BadRequestException('Rating does not exist');
+      }
 
-  remove(id: number) {
-    return `This action removes a #${id} rating`;
+      if (rating.userId !== userId) {
+        throw new BadRequestException('User is not the owner of the rating');
+      }
+    });
+    return this.ratingRep.delete(recipeId);
   }
 }
